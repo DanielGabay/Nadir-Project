6c7b69985bcaa48bc5cb70e628b2fb2bbbe1a5cc
DanielGabay <danielgabay5@gmail.com>
2019-05-03 12:54:38 +0300

    downloading and liknking jQuery to all html, updating addMember.html + addMember.js

--- src/js/addMember.js
+++ src/js/addMember.js
@@ -1,71 +1,69 @@
 const firestore = firebase.firestore();
 
-setGroups();
-const addForm = document.querySelector("#form-Add");
-
-addForm.addEventListener("submit", function () {
-    /* attach the html elements*/
-
-    const firstName = document.querySelector("#first-name").value;
-    const lastName = document.querySelector("#last-name").value;
-    const date = document.querySelector("#date").value;
-    const group = document.querySelector("#group").value;
-    const comments = document.querySelector("#comments").value; 
-    const school = document.querySelector("#school").value;
-    const phoneNum = document.querySelector("#phone-num").value;
-    const grade = document.querySelector("#grade").value; //
-    const parentPhoneNum = document.querySelector("#parent-phone-num").value;
-    const youthMovement = document.querySelector("#youth-movement").value;
-    const anotherEducation = document.querySelector("#another-education").value; //
-    const isInstructor = document.querySelector("#is-instructor").value;
-
-    /*the data of this user will store in this location*/
-    const Path = "Members/" + firstName + "-" + lastName;
-    const docRef = firestore.doc(Path); // pointer to the place we add the data
-
-    let personalTracking = [];
-    let financialMonitoring = [];
-
-    /* make the object to add ===> key : value */
-    docRef.set({
-        First: firstName,
-        Last: lastName,
-        Date: date,
-        Group: group,
-        School: school,
-        PhoneNum: phoneNum,
-        Grade: grade,
-        ParentPhoneNum: parentPhoneNum,
-        YouthMovement: youthMovement,
-        AnotherEducation: anotherEducation,
-        IsInstructor: isInstructor,
-        Comments: comments,
-        PersonalTracking :personalTracking,
-        FinancialMonitoring: financialMonitoring
-
-    }).then(function () {
-        console.log("status  saved");
-    }).catch(function (error) {
-        console.log("got error!!!", error)
+$(document).ready(function(){
+    setGroups();
+    
+    const addForm = document.querySelector("#form-Add");
+    addForm.addEventListener("submit", function () {
+        /* attach the html elements*/
+    
+        const firstName = document.querySelector("#first-name").value;
+        const lastName = document.querySelector("#last-name").value;
+        const date = document.querySelector("#date").value;
+        const group = document.querySelector("#group").value;
+        const comments = document.querySelector("#comments").value; 
+        const school = document.querySelector("#school").value;
+        const phoneNum = document.querySelector("#phone-num").value;
+        const grade = document.querySelector("#grade").value; //
+        const parentPhoneNum = document.querySelector("#parent-phone-num").value;
+        const youthMovement = document.querySelector("#youth-movement").value;
+        const anotherEducation = document.querySelector("#another-education").value; //
+        const isInstructor = document.querySelector("#is-instructor").value;
+    
+        /*the data of this user will store in this location*/
+        const Path = "Members/" + firstName + "-" + lastName;
+        const docRef = firestore.doc(Path); // pointer to the place we add the data
+    
+        let personalTracking = [];
+        let financialMonitoring = [];
+    
+        /* make the object to add ===> key : value */
+        docRef.set({
+            First: firstName,
+            Last: lastName,
+            Date: date,
+            Group: group,
+            School: school,
+            PhoneNum: phoneNum,
+            Grade: grade,
+            ParentPhoneNum: parentPhoneNum,
+            YouthMovement: youthMovement,
+            AnotherEducation: anotherEducation,
+            IsInstructor: isInstructor,
+            Comments: comments,
+            PersonalTracking :personalTracking,
+            FinancialMonitoring: financialMonitoring
+    
+        }).then(function () {
+            console.log("status  saved");
+        }).catch(function (error) {
+            console.log("got error!!!", error)
+        });
     });
 });
 
-/*load groups names from database*/
 function setGroups() {
     const Path = "groups/groups";
     const docRef = firestore.doc(Path); // pointer to the place we add the data
-    let str = '<select class="ui fluid dropdown" id="group">';
-    str += '<option disabled selected value value="">בחר קבוצה</option>';
+    let str = '<option disabled value="" selected value>בחר קבוצה</option>';
   
     docRef.get().then(function (doc) { //  onsnapshot will do it faster
         if (doc && doc.exists) {
             const theData = doc.data();
             var names = theData.names;
-            for (let i = 0; i < names.length; i++) {
+            for (let i = 0; i < names.length; i++)
                 str += '<option value="' + names[i] + '">' + names[i] + '</option>'
-            }
-            str +="</select>"
-            document.querySelector("#place-group").innerHTML = str;
+            $("#group").append(str);
         }
     });
     
