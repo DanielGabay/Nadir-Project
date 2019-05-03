51fb7a0cb0fdd18cbe848642178fd25c25dec82b
DanielGabay <danielgabay5@gmail.com>
2019-05-03 15:38:12 +0300

    editing addMember page

--- src/js/addMember.js
+++ src/js/addMember.js
@@ -1,32 +1,32 @@
 const firestore = firebase.firestore();
 
-$(document).ready(function(){
+/*when document is ready*/
+$(document).ready(function () {
     setGroups();
-    
-    const addForm = document.querySelector("#form-Add");
-    addForm.addEventListener("submit", function () {
-        /* attach the html elements*/
-    
-        const firstName = document.querySelector("#first-name").value;
-        const lastName = document.querySelector("#last-name").value;
-        const date = document.querySelector("#date").value;
-        const group = document.querySelector("#group").value;
-        const comments = document.querySelector("#comments").value; 
-        const school = document.querySelector("#school").value;
-        const phoneNum = document.querySelector("#phone-num").value;
-        const grade = document.querySelector("#grade").value; //
-        const parentPhoneNum = document.querySelector("#parent-phone-num").value;
-        const youthMovement = document.querySelector("#youth-movement").value;
-        const anotherEducation = document.querySelector("#another-education").value; //
-        const isInstructor = document.querySelector("#is-instructor").value;
-    
-        /*the data of this user will store in this location*/
+
+    $("#form-Add").submit(function (event) {
+        event.preventDefault();
+        /* attach html elements*/
+        const firstName = $("#first-name").val();
+        const lastName = $("#last-name").val();
+        const date = $("#date").val();
+        const group = $("#group").val();
+        const comments = $("#comments").val();
+        const school = $("#school").val();
+        const phoneNum = $("#phone-num").val();
+        const grade = $("#grade").val(); 
+        const parentPhoneNum = $("#parent-phone-num").val();
+        const youthMovement = $("#youth-movement").val();
+        const anotherEducation = $("#another-education").val();
+        const isInstructor = $("#is-instructor").val();
+
+        /*the data of this user will be stored at this location*/
         const Path = "Members/" + firstName + "-" + lastName;
         const docRef = firestore.doc(Path); // pointer to the place we add the data
-    
+
         let personalTracking = [];
         let financialMonitoring = [];
-    
+
         /* make the object to add ===> key : value */
         docRef.set({
             First: firstName,
@@ -41,46 +41,49 @@ $(document).ready(function(){
             AnotherEducation: anotherEducation,
             IsInstructor: isInstructor,
             Comments: comments,
-            PersonalTracking :personalTracking,
+            PersonalTracking: personalTracking,
             FinancialMonitoring: financialMonitoring
-    
         }).then(function () {
-            console.log("status  saved");
+            console.log("Member added!");
         }).catch(function (error) {
             console.log("got error!!!", error)
         });
-    });
+     });
+    
 });
 
+
+/*dynamicly set groups name at the drop-down select tag of groups (fetch data from the data base)*/
 function setGroups() {
     const Path = "groups/groups";
     const docRef = firestore.doc(Path); // pointer to the place we add the data
     let str = '<option disabled value="" selected value>בחר קבוצה</option>';
-  
+
     docRef.get().then(function (doc) { //  onsnapshot will do it faster
         if (doc && doc.exists) {
-            const theData = doc.data();
-            var names = theData.names;
-            for (let i = 0; i < names.length; i++)
-                str += '<option value="' + names[i] + '">' + names[i] + '</option>'
+            const groups = doc.data();
+            let groupsNames = groups.names;
+            for (let i = 0; i < groupsNames.length; i++)
+                str += '<option value="' + groupsNames[i] + '">' + groupsNames[i] + '</option>'
             $("#group").append(str);
         }
     });
+
+}
+
+function loading(id){
     
 }
 
 
-function deleteMemeber(firstName,lastName)
-{
-    const Path = "Users/" + firstName + "-" + lastName;
+function deleteMemeber(firstName, lastName) {
+    const Path = "Users/" + firstName + " " + lastName;
     const docRef = firestore.doc(Path); // pointer to the place we add the data
 
-    docRef.delete().then(function() {
+    docRef.delete().then(function () {
         console.log("Document successfully deleted!");
-    }).catch(function(error) {
+    }).catch(function (error) {
         console.error("Error removing document: ", error);
     });
-
-
 }
 
