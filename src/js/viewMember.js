de63c8a4b4add9f0d43865832b9b1ebc087f6907
guy <guuy1@hotmail.com>
2019-05-12 16:00:05 +0300

    edit and print buttons

--- src/js/viewMember.js
+++ src/js/viewMember.js
@@ -55,3 +55,62 @@ function setFields(doc){
   }
 
 }
+
+function change(){
+  let selectedPersonKey = sessionStorage.getItem('selectedPersonKey');
+  firestore.collection("Members").doc(selectedPersonKey).get()
+  .then(function(doc) {
+    if (doc.exists) {
+       const member = doc.data();
+       console.log("Document data:", doc.data());
+       editToSave(doc);
+       
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch(function(error) {
+    console.log("Error getting document:", error);
+});   
+}
+
+function editToSave(doc){
+  if (doc && doc.exists){
+    const member = doc.data();
+  if($("#edit-btn").text() == "עריכה"){
+    $("#edit-btn").text("שמור").append("<i class='save icon'></i>");
+    $("#first-name").removeAttr("readonly");
+    $("#last-name").removeAttr("readonly");
+    $("#date").removeAttr("readonly");
+    $("#group").removeAttr("readonly");
+    $("#comments").removeAttr("readonly");
+    $("#school").removeAttr("readonly");
+    $("#phone-num").removeAttr("readonly");
+    $("#grade").removeAttr("readonly");
+    $("#parent-phone-num").removeAttr("readonly");
+    $("#youth-movement").removeAttr("readonly");
+    $("#another-education").removeAttr("readonly");
+    $("#is-instructor").replaceWith("<select class='ui fluid dropdown' id='is-instructor' ><option value='false'>לא</option><option value='true'>כן</option></select>");
+    $("#is-instructor").val(member.IsInstructor);
+  }
+  else if($("#edit-btn").text() == "שמור"){
+    $("#edit-btn").text("עריכה").append("<i class='edit icon'></i>");
+    $("#first-name").attr("readonly", "");
+    $("#last-name").attr("readonly", "");
+    $("#date").attr("readonly", "");
+    $("#group").attr("readonly", "");
+    $("#comments").attr("readonly", "");
+    $("#school").attr("readonly", "");
+    $("#phone-num").attr("readonly", "");
+    $("#grade").attr("readonly", "");
+    $("#parent-phone-num").attr("readonly", "");
+    $("#youth-movement").attr("readonly", "");
+    $("#another-education").attr("readonly", "");
+    $("#is-instructor").replaceWith("<input readonly='' id='is-instructor'>");
+    if(member.IsInstructor == "false")
+      $("#is-instructor").val("לא");
+    else
+      $("#is-instructor").val("כן");
+  }
+}
+}
